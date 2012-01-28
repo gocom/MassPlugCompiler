b3857c16d3111b81023824d81469149a7de24fc9
Jukka Svahn <web@rahinaa.biz>
2012-01-28 22:29:20 +0200

    Added template.

---
+++ template/abc_plugin/abc_plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This is an example plugin.
+ *
+ * @author Jukka Svahn
+ * @copyright (c) 2012 Jukka Svahn
+ * @license GNU GPLv2
+ * @link https://github.com/gocom/MassPlugCompiler
+ *
+ */
+
+	function abc_example() {
+		return 'Hello world!';
+	}
+
+?>
