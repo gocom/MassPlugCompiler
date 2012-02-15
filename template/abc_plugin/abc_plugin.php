b3a9112bcd08170063da6e56a19f589e0c123edc
Jukka Svahn <web@rahinaa.biz>
2012-02-15 12:58:48 +0200

    More example-ish addresses.

--- template/abc_plugin/abc_plugin.php
+++ template/abc_plugin/abc_plugin.php
@@ -6,12 +6,12 @@
  * @author Jukka Svahn
  * @copyright (c) 2012 Jukka Svahn
  * @license GNU GPLv2
- * @link https://github.com/gocom/MassPlugCompiler
+ * @link http://example.com
  *
  */
 
 	function abc_example() {
-		return 'Hello world!';
+		return gTxt('abc_plugin_greeting');
 	}
 
 ?>
